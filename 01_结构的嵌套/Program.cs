d47f09a4c07c02a21f0884e5b580da3d8082251c
俊华 蔡 <1032031912@qq.com>
2020-09-24 09:35:24 +0800

    结构

---
+++ 01_结构的嵌套/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_结构的嵌套
+{
+    struct Student   //Student 结构
+    {
+        public struct Grade  //嵌套结构 (分数)
+        {
+            public string courseName;public double courseGrade;// 课程名称、分数
+            public Grade(string name, double grade)
+            {
+                courseName = name;courseGrade = grade;
+            }
+        }
+        public string studentID, studentName;//学号、姓名
+        public Grade[] grades;//分数(嵌套结构类型)
+        public Student(string id, string name)//学生结构的构造函数
+        {
+            studentID = id;studentName = name;grades = new Grade[3];
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
